c879b020baf8492996337d393faf141143f33f18
Kobi Gurkan <kobigurk@gmail.com>
2021-11-02 19:09:02 +0200

    feat: adds verification code

--- src/bin/verify-trusted-setup.rs
+++ src/bin/verify-trusted-setup.rs
@@ -1,4 +1,7 @@
+use ark_bls12_381::Fr;
+use ark_ec::AffineCurve;
 use prompt::{puzzle, welcome};
+use std::str::FromStr;
 use trusted_setup::data::puzzle_data;
 use trusted_setup::PUZZLE_DESCRIPTION;
 
@@ -7,5 +10,8 @@ fn main() {
     puzzle(PUZZLE_DESCRIPTION);
     let (_ts1, _ts2) = puzzle_data();
 
-    /* Your solution here! */
+    /* Your solution here! (s in decimal)*/
+    let s = Fr::from_str("0").unwrap();
+
+    assert_eq!(_ts1[0].mul(s), _ts1[1]);
 }
